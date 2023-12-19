4f28b310c021834dcb6fbe053a0bca61fdd1d294
Tyler Carbone <tcarbone073@gmail.com>
2023-12-19 14:46:24 +0000

    usePokemon hook gets one Pokemon

--- src/hooks/usePokemon.ts
+++ src/hooks/usePokemon.ts
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 
-import { CanceledError } from "../services/api-client";
+import { CanceledError, PokemonResource } from "../services/api-client";
 import pokemonService, { Pokemon } from "../services/pokemon-service";
 
-function usePokemon() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+function usePokemon(id: number) {
+  const [pokemon, setPokemon] = useState<Pokemon>();
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    const { request, cancel } = pokemonService.getAll<Pokemon>();
+    const { request, cancel } = pokemonService.getOne<PokemonResource>(id);
     request
       .then((response) => {
-        setPokemon(response.data);
+        setPokemon({
+          id: response.data.id,
+          name: response.data.name,
+          height: response.data.height,
+          weight: response.data.weight,
+          baseExperience: response.data.baseExperience,
+          abilityCount: 2,
+          spriteURL: response.data.sprites.front_default,
+        });
         setLoading(false);
       })
       .catch((error) => {
