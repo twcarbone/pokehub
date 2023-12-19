23f8d8991e1846674985587e0cbf0eb44b20fd09
Tyler Carbone <tcarbone073@gmail.com>
2023-12-19 14:42:16 +0000

    Add resource types to api client service

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,35 @@
 import axios, { CanceledError } from "axios";
 
+interface Resource {
+  id: number;
+}
+
+interface AbilityResource extends Resource {
+  name: string;
+  effect_entries: [
+    {
+      effect: string;
+      short_effect: string;
+    },
+  ];
+}
+
+interface PokemonResource extends Resource {
+  name: string;
+  baseExperience: number;
+  height: number;
+  isDefault: boolean;
+  order: number;
+  weight: number;
+  abilites: AbilityResource[];
+  sprites: {
+    front_default: string;
+  };
+}
+
 export default axios.create({
   baseURL: "https://pokeapi.co/api/v2",
 });
 
 export { CanceledError };
+export type { AbilityResource, PokemonResource };
