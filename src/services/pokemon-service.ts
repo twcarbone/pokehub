c8b49078e358c6592b12d334e70e7d396b12b41f
Tyler Carbone <tcarbone073@gmail.com>
2023-12-19 14:44:32 +0000

    Add attributes to Pokemon interface

--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -3,6 +3,11 @@ import create from "./http-service";
 export interface Pokemon {
   id: number;
   name: string;
+  height: number;
+  weight: number;
+  baseExperience: number;
+  abilityCount: number;
+  spriteURL: string;
 }
 
 export default create("/pokemon");
